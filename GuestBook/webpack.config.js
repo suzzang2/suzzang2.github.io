fca858a7c83263f164d18c7715478ba7fdf56b91
suzzang2 <subin030102@naver.com>
2024-05-28 20:59:56 +0900

    FEAT: webpack 완성

--- GuestBook/webpack.config.js
+++ GuestBook/webpack.config.js
@@ -1,3 +1,22 @@
+
 module.exports = {
     entry: "./index.js",
-}
+    output: {
+        path: __dirname,
+        filename: "build.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    },
+                },
+            },
+        ],
+    }
+};
+
